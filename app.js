f73cc928206633edc85c1e74792704cbb0e9f7dc
Dirk Verest <d.verest@gmail.com>
2020-07-10 15:33:15 -0400

    Local mysql connection

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 // Utility for working with file and directory paths
 const path = require("path");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-// const indexRouter = require("./routes/index");
-// const usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
 
 const app = express();
-
-app.use(logger("dev"));
-
-// GraphQL route handler
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: MyGraphQLSchema,
-    graphiql: true,
-  })
-);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -28,12 +16,20 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+
+// GraphQL Route handler
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: MyGraphQLSchema,
+//     graphiql: true,
+//   })
+// );
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
